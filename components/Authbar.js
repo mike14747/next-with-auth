9d1a825413a0fbe2c2ce76091d562f988d9ee6f6
mike14747 <mike4747@oh.rr.com>
2022-08-01 13:26:16 -0400

    Added the Authbar component to the Header.

---
+++ components/Authbar.js
@@ -0,0 +1,53 @@
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import { signOut, useSession } from 'next-auth/react';
+import Button from '../components/Button';
+
+import styles from '../styles/Authbar.module.css';
+
+const Authbar = () => {
+    const { data: session, status } = useSession();
+    const loading = status === 'loading';
+
+    const router = useRouter();
+
+    return (
+        <div className={styles.authbarContainer}>
+            {loading && <>Loading...</>}
+
+            {!session && !loading &&
+                <p>
+                    <Link href={`/login?url=${router.pathname}`}>
+                        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                        <a>Login</a>
+                    </Link>
+
+                    <span>&nbsp;/&nbsp;</span>
+
+                    <Link href={'/register'}>
+                        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                        <a>Register</a>
+                    </Link>
+                </p>
+            }
+
+            {session &&
+                <>
+                    <p className={styles.username}>
+                        <>User: </>
+                        <Link href="/profile">
+                            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                            <a>
+                                {session.user.name}
+                            </a>
+                        </Link>
+                    </p>
+
+                    <Button onClick={() => signOut({ redirect: false })} size="small" variant="text">Logout</Button>
+                </>
+            }
+        </div>
+    );
+};
+
+export default Authbar;
--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,28 @@
+import Link from 'next/link';
+import Authbar from './Authbar';
+
 import styles from '../styles/Header.module.css';
 
 export default function Header() {
     return (
         // to make the header full width, just omit the container class
         <div className={styles.header + ' container'}>
-            <p>This is the Header component.</p>
+            <div className={styles.leftDiv}>
+                <p>This is the Header component.</p>
+
+                <p>
+                    <Link href="/">
+                        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                        <a>
+                            Home
+                        </a>
+                    </Link>
+                </p>
+            </div>
+
+            <div className={styles.rightDiv}>
+                <Authbar />
+            </div>
         </div>
     );
 }
