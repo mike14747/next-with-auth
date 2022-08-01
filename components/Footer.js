0235550ed9fdac6103912470b654c6b7d56b719b
mike14747 <mike4747@oh.rr.com>
2022-08-01 11:46:37 -0400

    Began using the header and footer style modules in their components.

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,10 @@
+import styles from '../styles/Footer.module.css';
+
 export default function Footer() {
     return (
-        <>
+        // to make the footer full width, just omit the container class
+        <div className={styles.footer + ' container'}>
             <p>This is the Footer component.</p>
-        </>
+        </div>
     );
 }
--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,10 @@
+import styles from '../styles/Header.module.css';
+
 export default function Header() {
     return (
-        <>
+        // to make the header full width, just omit the container class
+        <div className={styles.header + ' container'}>
             <p>This is the Header component.</p>
-        </>
+        </div>
     );
 }
