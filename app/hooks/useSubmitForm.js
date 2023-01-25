5937a3cf77a11e1e62cb8fa24b8b93aeb608d0f1
mike14747 <mike4747@oh.rr.com>
2023-01-25 14:55:41 -0500

    Updated the useSubmitForm hook.

--- app/hooks/useSubmitForm.js
+++ app/hooks/useSubmitForm.js
@@ -2,7 +2,7 @@ import { useState, useCallback } from 'react';
 
 export default function useSubmitForm(url, body) {
     const [statusCode, setStatusCode] = useState(undefined);
-    const [isPending, setIsPending] = useState(true);
+    const [isSubmitting, setIsSubmitting] = useState(true);
 
     const handleSubmit = useCallback(() => {
         fetch(url, {
@@ -14,14 +14,14 @@ export default function useSubmitForm(url, body) {
         })
             .then(res => setStatusCode(res.status))
             .catch(error => console.error(error.name + ': ' + error.message))
-            .finally(() => setIsPending(false));
+            .finally(() => setIsSubmitting(false));
 
-        setIsPending(false);
+        setIsSubmitting(false);
     }, [url, body]);
 
     return {
         handleSubmit,
         statusCode,
-        isPending,
+        isSubmitting,
     };
 }
