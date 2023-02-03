82f2e8d0719eabb7a3bc18867142af60ed7e57a8
mike14747 <mike4747@oh.rr.com>
2023-02-03 13:52:39 -0500

    Disabled typescript-eslint in .eslintrc.json temporarily... until I figure out my optimized config.

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,6 +1,6 @@
-import NextAuth, { DefaultSession } from 'next-auth';
+import { DefaultSession } from 'next-auth';
 
-declare module "next-auth" {
+declare module 'next-auth' {
     interface User {
         role: string;
     }
@@ -10,7 +10,7 @@ declare module "next-auth" {
     }
 }
 
-declare module "next-auth/jwt" {
+declare module 'next-auth/jwt' {
     interface JWT {
         id: string;
         role: string;
