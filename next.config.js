ef3fd71fa58c9f5f9c98f75c644d2aec2201febc
mike14747 <mike4747@oh.rr.com>
2023-03-12 00:11:59 -0500

    Added security headers to next.config.js.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,57 @@
+const ContentSecurityPolicy = `
+    default-src 'self';
+    script-src 'self' 'unsafe-inline' 'unsafe-eval';
+    style-src 'self' 'unsafe-inline';
+    img-src * blob: data:;
+    media-src 'none';
+    connect-src *;
+    font-src 'self';
+`;
+
+const securityHeaders = [
+    {
+        key: 'Content-Security-Policy',
+        value: ContentSecurityPolicy.replace(/\n/g, ''),
+    },
+    {
+        key: 'Referrer-Policy',
+        value: 'origin-when-cross-origin',
+    },
+    {
+        key: 'X-Frame-Options',
+        value: 'DENY',
+    },
+    {
+        key: 'X-Content-Type-Options',
+        value: 'nosniff',
+    },
+    {
+        key: 'X-DNS-Prefetch-Control',
+        value: 'on',
+    },
+    {
+        key: 'Strict-Transport-Security',
+        value: 'max-age=31536000; includeSubDomains; preload',
+    },
+    {
+        key: 'Permissions-Policy',
+        value: '',
+    },
+];
+
 const nextConfig = {
     experimental: {
         appDir: true,
     },
     reactStrictMode: true,
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: securityHeaders,
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
